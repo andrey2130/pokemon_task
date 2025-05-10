6ff550b04aab5e3bfd869eb3856d6cfed770a499
Andrii <andriy.2130duda@gmail.com>
2025-05-10 22:18:54 +0300

    Remove unused Pokemon data sources, repositories, and related entities to streamline the codebase. This cleanup enhances maintainability and adheres to clean architecture principles by eliminating unnecessary components.

--- lib/feature/pokemon/domain/repositories/pokemon_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:pokemon_task/core/error_handling/failure.dart';
-import 'package:pokemon_task/feature/game/domain/entities/pokemon_entity.dart';
-
-abstract class PokemonRepository {
-  Future<Either<Failure, PokemonEntity>> getPokemonById(int id);
-  Future<Either<Failure, List<int>>> getRandomPokemonIds(int count);
-}
