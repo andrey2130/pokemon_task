59ee4291b6fa16bdb23658d45d59f4073a0c8c3d
Andrii <andriy.2130duda@gmail.com>
2025-05-14 20:02:39 +0300

    Remove HomePage widget and update import in MainNavigationPage to GamePage. This change enhances code organization and adheres to clean architecture principles.

--- lib/feature/game/presentation/pages/main_navigation_page.dart
+++ lib/feature/game/presentation/pages/main_navigation_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
-import 'package:pokemon_task/feature/game/presentation/pages/home_page.dart';
+import 'package:pokemon_task/feature/game/presentation/pages/game_page.dart';
 import 'package:pokemon_task/feature/profile/presentation/pages/profile_page.dart';
 import 'package:pokemon_task/feature/leaderboard/presentation/bloc/leaderboard_bloc.dart';
 import 'package:pokemon_task/feature/leaderboard/presentation/pages/leaderboard_page.dart';
